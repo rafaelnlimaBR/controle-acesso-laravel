a33501ec104cdb59a4c3d20758ab8bf4e37350ca
Tecvel <rafaelnlima@live.com>
2026-02-21 10:13:56 -0300

    servicos

--- resources/views/admin/contratos/includes/servicos.blade.php
+++ resources/views/admin/contratos/includes/servicos.blade.php
@@ -8,6 +8,7 @@
             <thead>
             <tr>
                 <th style="width: 15px" scope="col">#</th>
+                <th style="width: 10%" scope="col">Status</th>
                 <th style="width: 20%" scope="col">Serviço</th>
                 <th style="width: 20%" scope="col">Valor Bruto</th>
                 <th style="width: 20%" scope="col">Desconto</th>
@@ -19,18 +20,18 @@
             </thead>
             <tbody>
 
-            @foreach($contrato->historicos->map->servicos->flatten() as $s)
-                <tr class="align-middle">
-                    <td>{{$s->id}}</td>
-                    <td>{{$s->nome}}</td>
-                    <td>{{$s->pivot->valor_bruto}}</td>
-                    <td>{{$s->pivot->desconto}}</td>
-                    <td>{{$s->pivot->valor_liquido}}</td>
-                    <td>{{$s->pivot->cobrar}}</td>
-
-
-
-                </tr>
+            @foreach($contrato->historicos as $h)
+                @foreach($h->servicos as $s)
+                    <tr class="align-middle">
+                        <td>{{$s->id}}</td>
+                        <td>{{$h->status->nome}}</td>
+                        <td>{{$s->nome}}</td>
+                        <td>{{$s->pivot->valor_bruto}}</td>
+                        <td>{{$s->pivot->desconto}}</td>
+                        <td>{{$s->pivot->valor_liquido}}</td>
+                        <td>{{$s->pivot->cobrar}}</td>
+                    </tr>
+                @endforeach
             @endforeach
 
             </tbody>
