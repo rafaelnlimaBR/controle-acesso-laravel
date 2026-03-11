abdbc94d5090b179165f58f91160fe0834719793
Tecvel <rafaelnlima@live.com>
2026-03-11 16:25:37 -0300

    pdf

--- resources/views/admin/contratos/includes/pdf.blade.php
+++ resources/views/admin/contratos/includes/pdf.blade.php
@@ -10,7 +10,7 @@
                 <div class="col-12">
                     <h4>
                         <i class="fas fa-globe"></i> AdminLTE, Inc.
-                        <small class="float-right">Date: 2/10/2014</small>
+                        <small class="float-right">Data: {{\Carbon\Carbon::now()->format('d/m/Y ')}}</small>
                     </h4>
                 </div>
                 <!-- /.col -->
@@ -18,129 +18,150 @@
             <!-- info row -->
             <div class="row invoice-info">
                 <div class="col-sm-4 invoice-col">
-                    From
+
                     <address>
-                        <strong>Admin, Inc.</strong><br>
-                        795 Folsom Ave, Suite 600<br>
-                        San Francisco, CA 94107<br>
-                        Phone: (804) 123-5432<br>
-                        Email: info@almasaeedstudio.com
+                        <strong>Tecvel</strong><br>
+                        Rua Pinto Madeira, 750 - Centro<br>
+                        Fortaleza - Ceará<br>
+                        Whatsapp: (85) 987067785<br>
+                        Email: rafael@tecvelautomotiva.com.br
                     </address>
                 </div>
                 <!-- /.col -->
                 <div class="col-sm-4 invoice-col">
-                    To
+
                     <address>
-                        <strong>John Doe</strong><br>
-                        795 Folsom Ave, Suite 600<br>
-                        San Francisco, CA 94107<br>
-                        Phone: (555) 539-1037<br>
-                        Email: john.doe@example.com
+                        <strong>{{$contrato->cliente->name}}</strong><br>
+                        Contatos: {{$contrato->cliente->contatos->pluck('numero')->join(', ')}}<br>
+                        @if($contrato->veiculo != null)
+                            Veículo: {{$contrato->veiculo->modelo->nome }}<br>
+                            Montadora: {{$contrato->veiculo->modelo->montadora->nome }}<br>
+                            Placa: {{$contrato->veiculo->placa}}<br>
+
+                        @endif
+
                     </address>
                 </div>
                 <!-- /.col -->
                 <div class="col-sm-4 invoice-col">
-                    <b>Invoice #007612</b><br>
-                    <br>
-                    <b>Order ID:</b> 4F3S8J<br>
-                    <b>Payment Due:</b> 2/22/2014<br>
-                    <b>Account:</b> 968-34567
+
+                    <b>Ordem ID:</b> {{$contrato->id}}<br>
+                    <b>Data de início:</b> {{\Carbon\Carbon::parse($contrato->data_inicio)->format('d/m/Y ')}}<br>
+{{--                    <b>Account:</b> 968-34567--}}
                 </div>
                 <!-- /.col -->
             </div>
             <!-- /.row -->
 
             <!-- Table row -->
+            @if($contrato->historicos->map->servicos->count() >= 1)
+                <h5 style="border-top: 1px solid #d3d6d2; padding-top: 5px">Serviços</h5>
             <div class="row">
                 <div class="col-12 table-responsive">
                     <table class="table table-striped">
                         <thead>
                         <tr>
-                            <th>Qty</th>
-                            <th>Product</th>
-                            <th>Serial #</th>
+                            <th>Nome</th>
+                            <th>Valor</th>
+                            {{--<th>Serial #</th>
                             <th>Description</th>
-                            <th>Subtotal</th>
+                            <th>Subtotal</th>--}}
                         </tr>
                         </thead>
                         <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Call of Duty</td>
-                            <td>455-981-221</td>
-                            <td>El snort testosterone trophy driving gloves handsome</td>
-                            <td>$64.50</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>Need for Speed IV</td>
-                            <td>247-925-726</td>
-                            <td>Wes Anderson umami biodiesel</td>
-                            <td>$50.00</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>Monsters DVD</td>
-                            <td>735-845-642</td>
-                            <td>Terry Richardson helvetica tousled street art master</td>
-                            <td>$10.70</td>
-                        </tr>
-                        <tr>
-                            <td>1</td>
-                            <td>Grown Ups Blue Ray</td>
-                            <td>422-568-642</td>
-                            <td>Tousled lomo letterpress</td>
-                            <td>$25.99</td>
-                        </tr>
+                        @foreach($contrato->historicos->map->servicos->flatten() as $servico)
+                            <tr>
+                                <td>{{$servico->nome}}</td>
+                                <td>{{$servico->pivot->valor_liquido   }}</td>
+
+                            </tr>
+                        @endforeach
+
+
                         </tbody>
                     </table>
                 </div>
                 <!-- /.col -->
             </div>
+            @endif
             <!-- /.row -->
 
-            <div class="row">
-                <!-- accepted payments column -->
-                <div class="col-6">
-                    <p class="lead">Payment Methods:</p>
-                    <img src="../../dist/img/credit/visa.png" alt="Visa">
-                    <img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
-                    <img src="../../dist/img/credit/american-express.png" alt="American Express">
-                    <img src="../../dist/img/credit/paypal2.png" alt="Paypal">
+            <!-- Table row -->
+            @if($contrato->historicos->map->pecasavulsas->count() >= 1)
+                <h5 style="border-top: 1px solid #d3d6d2; padding-top: 5px">Peças</h5>
+                <div class="row">
+                    <div class="col-12 table-responsive">
+                        <table class="table table-striped">
+                            <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Valor</th>
+                                {{--<th>Serial #</th>
+                                <th>Description</th>
+                                <th>Subtotal</th>--}}
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($contrato->historicos->map->pecasAvulsas->flatten() as $peca)
+                                <tr>
+                                    <td>{{$peca->nome}}</td>
+                                    <td>{{$peca->valor_liquido   }}</td>
 
-                    <p class="text-muted well well-sm shadow-none" style="margin-top: 10px;">
-                        Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem
-                        plugg
-                        dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.
-                    </p>
-                </div>
-                <!-- /.col -->
-                <div class="col-6">
-                    <p class="lead">Amount Due 2/22/2014</p>
+                                </tr>
+                            @endforeach
 
-                    <div class="table-responsive">
-                        <table class="table">
-                            <tbody><tr>
-                                <th style="width:50%">Subtotal:</th>
-                                <td>$250.30</td>
-                            </tr>
-                            <tr>
-                                <th>Tax (9.3%)</th>
-                                <td>$10.34</td>
-                            </tr>
-                            <tr>
-                                <th>Shipping:</th>
-                                <td>$5.80</td>
-                            </tr>
-                            <tr>
-                                <th>Total:</th>
-                                <td>$265.24</td>
-                            </tr>
-                            </tbody></table>
+
+                            </tbody>
+                        </table>
                     </div>
+                    <!-- /.col -->
                 </div>
-                <!-- /.col -->
-            </div>
+            @endif
+
+
+                <div class="row">
+                    <!-- accepted payments column -->
+                    <div class="col-6">
+                        <p class="lead">Payment Methods:</p>
+                        <img src="../../dist/img/credit/visa.png" alt="Visa">
+                        <img src="../../dist/img/credit/mastercard.png" alt="Mastercard">
+                        <img src="../../dist/img/credit/american-express.png" alt="American Express">
+                        <img src="../../dist/img/credit/paypal2.png" alt="Paypal">
+
+                        <p class="text-muted well well-sm shadow-none" style="margin-top: 10px;">
+                            Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles, weebly ning heekya handango imeem
+                            plugg
+                            dopplr jibjab, movity jajah plickers sifteo edmodo ifttt zimbra.
+                        </p>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-6">
+                        <p class="lead">Amount Due 2/22/2014</p>
+
+                        <div class="table-responsive">
+                            <table class="table">
+                                <tbody><tr>
+                                    <th style="width:50%">Subtotal:</th>
+                                    <td>$250.30</td>
+                                </tr>
+                                <tr>
+                                    <th>Tax (9.3%)</th>
+                                    <td>$10.34</td>
+                                </tr>
+                                <tr>
+                                    <th>Shipping:</th>
+                                    <td>$5.80</td>
+                                </tr>
+                                <tr>
+                                    <th>Total:</th>
+                                    <td>$265.24</td>
+                                </tr>
+                                </tbody></table>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+                </div>
+
             <!-- /.row -->
 
             <!-- this row will not appear when printing -->
